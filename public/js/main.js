9c99ad99707f02b58fb1fa127b9f26500794dc7c
SaraKoops <sarakoops@gmail.com>
2016-02-10 17:22:59 +0100

    setTimeout function

--- public/js/main.js
+++ public/js/main.js
@@ -2,32 +2,40 @@ $(document).ready(function(){
 
 	$('#results').hide();
 
+	var timer;
 
 	$('#search').on('keyup', function(){ // the search bar is has id# -- on 'keyup' an element is sent when the user releases a key on the keyboard.
 
-     var zoekterm = {search: $('#search').val()}; // puts what is typed into searchbar as an object with key search and value 'val' --> stored in variable zoekterm; 
-     //This data is then sent to the server.
+	clearTimeout(timer);
 
-        
-		$.ajax ({ // ajax request sending to the server -- shortcut in jquery is  $.get('/searching',typing, function(data) {}).
-		    url: '/searching',
-		    data: zoekterm,
-		   	error: function () {
+	timer = setTimeout(function(){ 
 
-		   		consonle.log('werkt niet looser');
-		   	}, 
-		   	success: function(data){ // callback function. De parameter krijgt de functie uit app.js file door callback functie.
 
-		   		console.log('testtesttest')
-				$('#results').html(data);
-	    	},
+    	var zoekterm = {search: $('#search').val()}; // puts what is typed into searchbar as an object with key search and value 'val' --> stored in variable zoekterm; 
+     	//This data is then sent to the server.
 
-	    	type: 'GET'
-			
-		});
+
+			$.ajax ({ // ajax request sending to the server -- shortcut in jquery is  $.get('/searching',typing, function(data) {}).
+				url: '/searching',
+				data: zoekterm,
+				error: function () {
+
+					consonle.log('werkt niet looser');
+				}, 
+			   	success: function(data){ // callback function. De parameter krijgt de functie uit app.js file door callback functie.
+
+			   		console.log('testtesttest')
+			   		$('#results').html(data);
+			   	},
+
+			   	type: 'GET'
+
+			});
 
 
 		$('#results').show();
+
+	},3000)
 
 	});
 
